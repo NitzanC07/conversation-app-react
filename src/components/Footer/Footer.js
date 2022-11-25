471a2b680278c2640281c1b5f1cd382ada89b097
Nitzan Cohen <nitzancohen@hotmail.com>
2022-11-25 02:37:03 +0200

    Added clock and date features to header

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,12 @@ import './footer.css';
 import './__text/footer__text.css';
 
 function Footer(props) {
+
+    const currentYear = new Date().getFullYear()   
+
     return(
         <footer className="footer">
-            <p className="footer__text">Developed by Nitzan Cohen &#169; 2022</p>
+            <p className="footer__text">Developed by Nitzan Cohen &#169; {currentYear}</p>
             <p className="footer__text">Beta version</p>
         </footer>
     )
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,37 @@
 import './header.css';
 import './__title/header__title.css';
+import './__time/header__time.css';
+import './__content/header__content.css';
+import { useEffect, useState } from 'react';
 
 function Header(props) {
+
+    const [date, setDate] = useState(String(new Date().getDate()).padStart(2, '0'))
+    const [month, setMonth] = useState(String(new Date().getMonth() + 1).padStart(2, '0'))
+    const [year, setYear] = useState(String(new Date().getFullYear()).padStart(2, '0'))
+    const [hour, setHour] = useState(String(new Date().getHours()).padStart(2, '0'));
+    const [minutes, setMinutes] = useState(String(new Date().getMinutes()).padStart(2, '0'));
+
+    useEffect(() => {
+        setInterval(() => tick(), 15*1000);
+    }, [])
+
+    function tick() {
+        setHour(String(new Date().getHours()).padStart(2, '0'));
+        setMinutes(String(new Date().getMinutes()).padStart(2, '0'));
+        setDate(String(new Date().getDate()).padStart(2, '0'));
+        setMonth(String(new Date().getMonth() + 1).padStart(2, '0'));
+        setYear(String(new Date().getFullYear()).padStart(2, '0'));
+    }
+
     return(
         <header className="header">
             <p className="header__title">מקליד את דעתי</p>
-            <p className="header__title header__time">שעה</p>
+            <div className='header__time'>
+                <p className="header__content">{date}/{month}/{year}</p>
+                <p className="header__content">{hour}:{minutes}</p>
+            </div>
+            
         </header>
     )
 }
