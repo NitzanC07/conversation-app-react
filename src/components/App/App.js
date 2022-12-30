a7413e3f4a435d54c61e17b1164a5ea954d0eca0
Nitzan Cohen <nitzancohen@hotmail.com>
2022-12-30 15:16:51 +0200

    Improve UX by preventing refresh the page without save the data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,21 @@ import './app.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', alertUser);
+    return () => {
+      window.removeEventListener('beforeunload', alertUser);
+    }
+  }, []);
+
+  const alertUser = (e) => {
+    e.preventDefault();
+    e.returnValue = '';
+  }
 
   return (
     <div className="app">
