560c5be6094c04874edb0e221e6c4be1a0374aad
Nitzan Cohen <nitzancohen@hotmail.com>
2023-07-16 09:41:46 +0300

    improve featrue free writing

--- src/components/PopupFreeWriting/PopupFreeWriting.js
+++ src/components/PopupFreeWriting/PopupFreeWriting.js
@@ -1,9 +1,16 @@
 import './styles.css';
 import Popup from "../Popup/Popup";
+import { useState } from 'react';
 // import { useEffect, useRef, useState } from 'react';
 
 function PopupFreeWriting(props) {
 
+    const [text, setText] = useState('');
+
+    function saveWriting(e) {
+        e.preventDefault();
+        console.log(text);
+    }
     return(
         <Popup 
             name="free-writing"
@@ -11,10 +18,15 @@ function PopupFreeWriting(props) {
             isOpen={props.isOpen}
             onClose={props.onClose}
         >
-            <form className='free-writing-form'>
-                <label for='writingArea'>כתוב כל מה שעולה לך לראש...</label>
-                <textarea id='writingArea' cols={15} />
-                <button type='submit'>שמור כתיבה</button>
+            <form className='free-writing-form' onSubmit={saveWriting}>
+                <label for='writingArea' className='popup__info__text'>כתוב כל מה שעולה לך לראש...</label>
+                <textarea 
+                    id='writingArea' 
+                    rows={10} 
+                    value={text || ''}
+                    onChange={(e) => setText(e.target.value)} 
+                />
+                <button className='save-button' type='submit'>שמור כתיבה</button>
             </form>
         </Popup>
     )
