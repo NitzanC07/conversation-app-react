628d8d7c81786995d5e0dd6a4ade4d53c75c8632
Nitzan Cohen <nitzancohen@hotmail.com>
2023-07-30 11:58:34 +0300

    Feature: typing trianing

--- src/components/PopupTypingTraining/PopupTypingTraining.js
+++ src/components/PopupTypingTraining/PopupTypingTraining.js
@@ -1,9 +1,15 @@
 import './styles.css';
 import Popup from "../Popup/Popup";
-// import { useState } from 'react';
+import { useState } from 'react';
 // import { useEffect, useRef, useState } from 'react';
 
 function PopupTypingTraining(props) {
+
+    const [stage, setStage] = useState(0);
+
+    const startExercise = (stage) => {
+        setStage(stage)
+    }
 
     return(
         <Popup 
@@ -13,7 +19,14 @@ function PopupTypingTraining(props) {
             isOpen={props.isOpen}
             onClose={props.onClose}
         >
-            
+            <div className='trainingPlans'>
+                <button className='stage' type='button' onClick={() => startExercise(1)}>שלב 1</button>
+                <button className='stage' type='button' onClick={() => startExercise(2)}>שלב 2</button>
+                <button className='stage' type='button' onClick={() => startExercise(3)}>שלב 3</button>
+            </div>
+            <div className='trainingBox'>
+                אתה נמצא בשלב {stage}
+            </div>
         </Popup>
     )
 }
